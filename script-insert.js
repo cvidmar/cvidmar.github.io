07e8e33a2be8ea9d42cfbd36615a87a01a6781fd
Cristian Vidmar <cristian.vidmar@icloud.com>
2023-03-03 10:49:18 +0100

    msgapps

--- script-insert.js
+++
@@ -1,6 +0,0 @@
-((event) => {  
-  const elem = document.getElementById('script-place');
-  console.log(elem);
-  elem.innerHTML = 'This is content coming from a third-party script. <em>Have a nice day!</em>';
-  elem.style.background = 'yellow';
-})();
